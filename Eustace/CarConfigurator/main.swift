47baa9c3558d00f1245c9d1bc0a84f1971b1ea95
max0403 <48206515+max0403@users.noreply.github.com>
2020-11-14 14:42:31 +0100

    added command line tool as an example of framework use

---
+++ Eustace/CarConfigurator/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  CarConfigurator
+//
+//  Created by Massimiliano Faustini on 14/11/2020.
+//  Copyright © 2020 Massimiliano Faustini. All rights reserved.
+//
+
+import Foundation
+import Eustace
+
+print("Hello, World!")
+
