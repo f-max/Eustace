fa3085d9779950c749a1e43b5bd2e7ca206dc85e
max0403 <48206515+max0403@users.noreply.github.com>
2020-11-14 14:38:57 +0100

    created framework project

---
+++ Eustace/Eustace/Eustace.h
@@ -0,0 +1,19 @@
+//
+//  Eustace.h
+//  Eustace
+//
+//  Created by Massimiliano Faustini on 14/11/2020.
+//  Copyright © 2020 Massimiliano Faustini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Eustace.
+FOUNDATION_EXPORT double EustaceVersionNumber;
+
+//! Project version string for Eustace.
+FOUNDATION_EXPORT const unsigned char EustaceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Eustace/PublicHeader.h>
+
+
